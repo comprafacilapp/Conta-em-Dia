499f59098ebf2f33d7648a7962c7d077bb4cc2ab
comprafacilapp <nascimentojacinto6@gmail.com>
2026-08-29 02:09:38 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "conta-em-dia-v1";
+const CACHE_NAME = "conta-em-dia-v2";
 
 const ARQUIVOS = [
   "./",
@@ -9,12 +9,27 @@ const ARQUIVOS = [
 ];
 
 self.addEventListener("install", function(event) {
-
+self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(function(cache) {
         return cache.addAll(ARQUIVOS);
       })
+  );
+
+});
+self.addEventListener("activate", function(event) {
+self.clients.claim();
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
   );
 
 });
